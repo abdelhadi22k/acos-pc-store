bc89505c54fbcb34fba72ada2aabe413949a0c55
abdelhadi22k <115563437+abdelhadi22k@users.noreply.github.com>
2024-08-27 16:44:43 +0100

    Update config.js

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,7 +3,7 @@ var domain
 if(process.env.NODE_ENV === 'production'){
 
 }else{
-    domain = https://acos-pc-store-beck-end-1.vercel.app/
+    domain = "https://acos-pc-store-beck-end-1.vercel.app"
 }
 
 export default domain;
